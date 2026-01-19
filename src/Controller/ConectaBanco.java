4fbc8c3cc9817a088560da64d378d6fe81e2a82a
prwilliam <nwillian533@gmail.com>
2026-01-19 08:43:06 -0300

    primeiro commit
    Iniciando o projeto - Interface gráfica, Interface Principal

---
+++ src/Controller/ConectaBanco.java
@@ -0,0 +1,40 @@
+package Controller;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Engenheiro de Sistemas William Nascimento
+ */
+public class ConectaBanco {
+    public Statement stm ;
+    public ResultSet rs ;
+    private String driver = "org.postgresql.Driver";
+    private String caminho = "jdbc:postgresql://localhost:5433/controlecomercial";
+    private String usuario = "postgres";
+    private String senha = "@mmde0566ev";
+    public Connection conn;
+
+    public void conexaoComBancoDados() {
+        try {
+            System.setProperty("jdbc.Drivers", driver);
+            conn = DriverManager.getConnection(caminho, usuario, senha);
+            JOptionPane.showMessageDialog(null, "Conexão efetuada com Sucesso!");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao tentar se conectar com o"
+                    + " banco de Dados!verifique a conexão...\n" + ex.getMessage());
+        }
+    }
+
+      
+  
+    public void desconectar() {
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao desconectar-se do Banco de Dados...\n"
+                    + ex.getMessage());
+        }
+    }
+}
---
+++ src/sistemacomercial/SistemaComercial.java
@@ -0,0 +1,21 @@
+
+package sistemacomercial;
+
+import View.frmPrincipal;
+
+/**
+ *
+ * @author prWilliam Nascimento
+ */
+public class SistemaComercial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frmPrincipal principal = new frmPrincipal();
+        principal.setVisible(true);
+    }
+    
+}
